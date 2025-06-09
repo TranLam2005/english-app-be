55cc9df92c5a84a8e5f6c9632b904bb58b10032f
tranlam <tranlam27052005@gmail.com>
2025-06-09 21:08:00 +0700

    feat change port

--- src/main/java/com/example/BackEnd/BackEndApplication.java
+++ src/main/java/com/example/BackEnd/BackEndApplication.java
@@ -1,6 +1,9 @@
 package com.example.BackEnd;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class BackEndApplication {
@@ -8,5 +11,4 @@ public class BackEndApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndApplication.class, args);
 	}
-
 }
